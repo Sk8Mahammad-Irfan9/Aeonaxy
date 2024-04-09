951e4dcd54ddb1a792b6e4f066bf50cce721eabc
Sk Mahammad Irfan <109087483+Sk8Mahammad-Irfan9@users.noreply.github.com>
2024-04-09 16:21:19 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,7 +6,13 @@ const connectDb = require("./db/connectDb");
 
 const app = express();
 app.use(express.json({ limit: "50mb" }));
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://aeonaxy-frontend.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
 // running port
